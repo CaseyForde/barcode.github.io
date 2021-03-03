023e962f233b49f84fb8e93fc19f1eea6597114c
CaseyForde <3020backup@gmail.com>
2021-03-02 22:28:56 -0400

    'BarcodeComponentTesting'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { BarcodeFormat, Result } from '@zxing/library';
+import { BrowserMultiFormatOneDReader } from '@zxing/browser';
+import Quagga from 'Quagga'
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Barcode';
+export class AppComponent implements OnInit,AfterViewInit{
+  @ViewChild('scanner') scanner: ZXingScannerComponent
+
+  permission:boolean = false;
+  enabled:boolean = false;
+  title: string;
+  torch = false;
+  failures = 0
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResult: Result;
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  ngAfterViewInit(){
+    this.scannerInit()
+  }
+
+  onClick(){
+    console.log(this.availableDevices)
+  }
+
+
+  // onFileSelected(event){
+  //   if (event.target.files && event.target.files[0]) {
+  //     this.file = event.target.files[0];
+  //     const file = event.target.files[0];
+  //     const reader = new FileReader();
+  //     reader.onload = () => this.imageSrc = reader.result;
+  //     reader.readAsDataURL(file);
+  //   }
+  // }
+
+  onDetect(result: string): void {
+    alert(result);
+  }
+
+  onTorchCompatible(event){
+    console.log(event,'torch')
+  }
+
+  camerasFoundHandler(event){
+    console.log(event,'cameras found')
+  }
+
+  camerasNotFoundHandler(event){
+    console.log('Not cammeras found')
+  }
+  scanSuccessHandler(event){
+
+  }
+
+  scanErrorHandler(event){
+
+  }
+  scanFailureHandler(event){
+    console.log("Failure")
+    this.failures++
+  }
+
+  scannerInit() {
+
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.hasDevices = true;
+      this.availableDevices = devices;
+      this.currentDevice = null;
+
+     // this.compareWith = this.compareWithFn;
+      this.onDeviceSelectChange(devices[devices.length - 1].deviceId);
+    });
+
+    this.scanner.camerasNotFound.subscribe(() => this.hasDevices = false);
+    this.scanner.scanComplete.subscribe((result: Result) => this.qrResult = result);
+    this.scanner.permissionResponse.subscribe((perm: boolean) => this.hasPermission = perm);
+  }
+
+  onDeviceSelectChange(selected: string) {
+
+    for (const device of this.availableDevices) {
+        if (device.deviceId === selected) {
+          this.currentDevice = device;
+        }
+      }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgBarcodeDetectorModule } from 'ng-barcode-detector';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ZXingScannerModule,
+    AppRoutingModule,
+    NgBarcodeDetectorModule,
+    BrowserAnimationsModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
