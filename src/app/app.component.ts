3a7383b702bc15df8d0d5ebb5f63286ff7ca0620
CaseyForde <3020backup@gmail.com>
2021-03-03 23:25:14 -0400

    made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,36 @@ import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 import { BehaviorSubject } from 'rxjs';
 import { BrowserCodeReader, HTMLCanvasElementLuminanceSource, HybridBinarizer, BinaryBitmap } from '@zxing/library';
 
-ZXingScannerComponent.prototype.getAnyVideoDevice = (): Promise<MediaStream> => {
-  return navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      width: { min: 640, ideal: 1920 },
-      height: { min: 400, ideal: 1080 },
-      aspectRatio: { ideal: 1.7777777778 }
-    }
-  });
+BrowserCodeReader.prototype.createBinaryBitmap = function (mediaElement) {
+  const captureCanvasContext = this.getCaptureCanvasContext(mediaElement);
+  this.drawImageOnCanvas(captureCanvasContext, mediaElement);
+  const captureCanvas = this.getCaptureCanvas(mediaElement);
+
+  const allWidth = captureCanvas.width;
+  const allHeight = captureCanvas.height;
+  const squareSize = Math.min(allWidth, allHeight) / 2;
+  const top = (allHeight - squareSize) / 2;
+  const left = (allWidth - squareSize) / 2;
+
+  document.getElementById('scan-area').style.width = `${squareSize}px`;
+  document.getElementById('scan-area').style.height = `${squareSize}px`;
+
+  const canvas = document.createElement('canvas');
+  canvas.width = squareSize;
+  canvas.height = squareSize;
+  const ctx = canvas.getContext('2d');
+  ctx.rect(0, 0, squareSize, squareSize);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  ctx.putImageData(captureCanvasContext.getImageData(left, top, squareSize, squareSize), 0, 0);
+
+  const luminanceSource = new HTMLCanvasElementLuminanceSource(canvas);
+  const hybridBinarizer = new HybridBinarizer(luminanceSource);
+
+  return new BinaryBitmap(hybridBinarizer);
 };
 
-BrowserCodeReader.prototype.decodeFromVideoDevice = async function(
-  deviceId: string | undefined,
-  previewElem: string | HTMLVideoElement | undefined,
-  callbackFn: any,
-): Promise<any> {
-
-  // We had to comment this out because it is a private method...
-  // BrowserCodeReader.checkCallbackFnOrThrow(callbackFn);
-
-  let videoConstraints: MediaTrackConstraints;
-
-  if (!deviceId) {
-    videoConstraints = { facingMode: 'environment' };
-  } else {
-    videoConstraints = {
-      deviceId: { exact: deviceId },
-      width: { min: 640, ideal: 1920 },
-      height: { min: 400, ideal: 1080 },
-      aspectRatio: { ideal: 1.7777777778 }
-    };
-  }
-
-  const constraints: MediaStreamConstraints = { video: videoConstraints };
-
-  return await this.decodeFromConstraints(constraints, previewElem, callbackFn);
-};
 
 @Component({
   selector: 'app-root',
@@ -79,7 +72,7 @@ export class AppComponent implements OnInit,AfterViewInit{
   }
 
   ngAfterViewInit(){
-
+    this.scanBarcode()
   }
   clearResult(): void {
     this.qrResultString = null;
@@ -116,4 +109,119 @@ export class AppComponent implements OnInit,AfterViewInit{
     this.tryHarder = !this.tryHarder;
   }
 
+  scanBarcode(){
+    Quagga.init(
+      {
+        inputStream: {
+          type: 'LiveStream',
+          constraints: {
+            width: { min: 100},
+            height: { min: 50},
+            aspectRatio: { min: 4 / 3, max: 16 / 9 },
+            //width: 800,
+            //height: 600,
+            //aspectRatio: 4 / 3,
+            facingMode: 'environment' // or user
+          },
+          area: {
+            // defines rectangle of the detection/localization area
+            top: '0%', // top offset
+            right: '0%', // right offset
+            left: '0%', // left offset
+            bottom: '0%' // bottom offset
+          }
+        },
+        frequency:'full',
+        locator: {
+          patchSize: 'medium',
+          halfSample: true
+        },
+        numOfWorkers: 8,
+        decoder: {
+          readers: [
+            // 'code_39_reader',
+            'ean_reader',
+            'ean_8_reader',
+            // 'code_128_reader',
+            //'code_39_vin_reader'
+            //'codabar_reader',
+            // 'upc_reader'
+            //'upc_e_reader',
+            //'i2of5_reader'
+          ]
+        },
+        locate: true
+      },
+      function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        Quagga.start();
+      }
+    );
+    Quagga.onDetected(this.onDetected);
+    Quagga.onProcessed(this.onProcessed);
+  }
+
+  onDetected (result) {
+    //add check for barcode from scan; use the getitembybarcode function
+    var countDecodedCodes=0, err=0;
+    result.codeResult.decodedCodes.forEach(element => {
+      if (element.error!=undefined) {
+        countDecodedCodes++;
+        err+=parseFloat(element.error);
+      }
+    });
+    if (err/countDecodedCodes < 0.1) {
+      console.log(result)
+
+      this.qrResultString = result.codeResult.code
+      alert(this.qrResultString)
+    } else {
+      console.log("we here?")
+    }
+  }
+
+
+  private onProcessed(result: any) {
+    // let drawingCtx = Quagga.canvas.ctx.overlay,
+    //   drawingCanvas = Quagga.canvas.dom.overlay;
+
+    // if (result) {
+    //   if (result.boxes) {
+    //     drawingCtx.clearRect(
+    //       0,
+    //       0,
+    //       parseInt(drawingCanvas.getAttribute('width'), 10),
+    //       parseInt(drawingCanvas.getAttribute('height'), 10)
+    //     );
+    //     result.boxes
+    //       .filter(function(box) {
+    //         return box !== result.box;
+    //       })
+    //       .forEach(function(box) {
+    //         Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+    //           color: 'green',
+    //           lineWidth: 2
+    //         });
+    //       });
+    //   }
+
+    //   if (result.box) {
+    //     Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+    //       color: '#00F',
+    //       lineWidth: 2
+    //     });
+    //   }
+
+    //   if (result.box) {
+    //     Quagga.ImageDebug.drawPath(
+    //       result.line,
+    //       { x: 'x', y: 'y' },
+    //       drawingCtx,
+    //       { color: 'red', lineWidth: 3 }
+    //     );
+    //   }
+    // }
+  }
 }
