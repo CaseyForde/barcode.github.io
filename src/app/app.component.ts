a92be556b345ad76a860d279f195980717a37bad
CaseyForde <3020backup@gmail.com>
2021-03-04 14:25:58 -0400

    made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,8 +51,11 @@ export class AppComponent implements OnInit,AfterViewInit{
   formatsEnabled: BarcodeFormat[] = [
     BarcodeFormat.CODE_128,
     BarcodeFormat.EAN_13,
+    BarcodeFormat.UPC_A,
+    BarcodeFormat.UPC_E
   ];
-  value = 'https://caseyforde.github.io/barcode.github.io/'
+
+
   hasDevices: boolean;
   hasPermission: boolean;
   detectionHash = {}
@@ -72,7 +75,7 @@ export class AppComponent implements OnInit,AfterViewInit{
   }
 
   ngAfterViewInit(){
-    this.scanBarcode()
+
   }
   clearResult(): void {
     this.qrResultString = null;
@@ -107,147 +110,6 @@ export class AppComponent implements OnInit,AfterViewInit{
 
   toggleTryHarder(): void {
     this.tryHarder = !this.tryHarder;
-  }
-
-  scanBarcode(){
-    const constraints = {
-      width: {
-          min: 640,
-      },
-      height: {
-          min: 480,
-      },
-      aspectRatio: {
-          min: 1,
-          max: 2,
-      }
-  };
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          constraints: {
-            width: '800',
-            height: '800',
-            facingMode: 'environment', // or user
-          },
-        },
-        frequency:'full',
-        locator: {
-          patchSize: 'medium',
-          halfSample: true,
-        },
-        numOfWorkers: window.navigator.hardwareConcurrency || 2,
-        locate: true,
-
-        decoder: {
-          readers: ['upc_reader', 'ean_reader']
-        }
-      },
-      function(err) {
-        if (err) {
-          return console.log(err);
-        }
-        Quagga.start();
-      }
-    );
-    Quagga.onDetected(this.onDetected);
-    Quagga.onProcessed(this.onProcessed);
-
-  }
-
-
-     //https://github.com/serratus/quaggaJS/issues/237#issuecomment-389667599
-  private _getMedian(arr: number[]): number {
-    arr.sort((a, b) => a - b);
-    const half = Math.floor( arr.length / 2 );
-    if (arr.length % 2 === 1) // Odd length
-      return arr[ half ];
-    return (arr[half - 1] + arr[half]) / 2.0;
-  }
-
-  onDetected (result) {
-    var countDecodedCodes=0, err=0;
-    result.codeResult.decodedCodes.forEach(element => {
-      if (element.error!=undefined) {
-        countDecodedCodes++;
-        err+=parseFloat(element.error);
-      }
-    });
-    if (err/countDecodedCodes < 0.5) {
-      // console.log(result)
-      // this.qrResultString = result.codeResult.code
-      // alert(this.qrResultString)
-        const errors: number[] = result.codeResult.decodedCodes
-          .filter(_ => _.error !== undefined)
-          .map(_ => _.error);
-        var median
-        errors .sort((a, b) => a - b);
-        const half = Math.floor( errors .length / 2 );
-        if (errors .length % 2 === 1) {// Odd length
-          median =  errors[ half ];
-        }
-        median =  (errors[half - 1] + errors[half]) / 2.0;
-        if (median < 0.10){
-          console.log(result)
-          console.log(err/countDecodedCodes)
-          this.qrResultString = result.codeResult.code
-          alert(this.qrResultString)
-        }
-        else{}
-          // probably wrong
-
-    } else {
-      console.log("we here?")
-    }
-
-
-  }
-
-
-  private onProcessed(result: any) {
-    (result: {
-      boxes: { filter: (arg0: (box: any) => boolean) => { forEach: (arg0: (box: any) => void) => void } };
-      box: any;
-      codeResult: { code: any };
-      line: any;
-      }) => {
-      const drawingCtx = Quagga.canvas.ctx.overlay;
-      const drawingCanvas = Quagga.canvas.dom.overlay;
-
-      if (result) {
-          if (result.boxes) {
-              drawingCtx.clearRect(
-                  0,
-                  0,
-                  parseInt(drawingCanvas.getAttribute('width'), 10),
-                  parseInt(drawingCanvas.getAttribute('height'), 10)
-              );
-              result.boxes
-                  .filter(box => box !== result.box)
-                  .forEach(box => {
-                  Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-                      color: 'lightblue',
-                      lineWidth: 3
-                  });
-                  });
-          }
-
-          if (result.box) {
-              Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-                  color: 'lightgreen',
-                  lineWidth: 3
-              });
-          }
-
-          if (result.codeResult && result.codeResult.code) {
-              Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, {
-                  color: 'red',
-                  lineWidth: 3
-              });
-          }
-      }
-      }
   }
 
 }
